783d026686e1c29b570ccf45158a36eab7404c17
ENDALE LEGESSE <101863853+Endale24@users.noreply.github.com>
2023-07-08 01:38:05 +0800

    changes made

---
+++ react-shopping-store-website.netlify.app/static/js/components/Layout/index.js
@@ -0,0 +1,17 @@
+// components
+import Navbar from "./Navbar";
+
+const Layout = ({
+    children
+}) => {
+    return ( <
+        >
+        <
+        Navbar / > {
+            children
+        } <
+        />
+    );
+};
+
+export default Layout;
---
+++ react-shopping-store-website.netlify.app/static/js/services/getAllProducts.js
@@ -0,0 +1,9 @@
+import http from "./http";
+
+const GetAllProducts = async () => {
+    const result = await http.get(`/products`);
+    return result.data;
+};
+export {
+    GetAllProducts
+};
